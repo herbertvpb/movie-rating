78fd9ce78107991f707c854be71a31edcfbd65b5
herbert <herbert.vpb@gmail.com>
2020-10-13 08:29:17 -0300

    initial structure

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import Detail from '../pages/Detail';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Home} />
+    <Route path="/movie-detail" component={Detail} />
+  </Switch>
+);
+
+export default Routes;
